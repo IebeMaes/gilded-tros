3369936b59029f9e1e96eaec5cac13e234334ad6
IebeMaes <Iebe.maes@outlook.com>
2023-11-25 15:45:32 +0100

    toevoegen comments om code beter te begrijpen

--- Java/src/main/java/com/gildedtros/GildedTros.java
+++ Java/src/main/java/com/gildedtros/GildedTros.java
@@ -8,27 +8,41 @@ class GildedTros {
     }
 
     public void updateQuality() {
+
+        // Loop door elk item in de items array
         for (int i = 0; i < items.length; i++) {
+
+            // Kwaliteit verminderen voor alle items, uitgezonderd legendarisch item (B-DAWG), Good wine en Backstage passes
             if (!items[i].name.equals("Good Wine")
                     && !items[i].name.equals("Backstage passes for Re:Factor")
-                    && !items[i].name.equals("Backstage passes for HAXX"))
-            {
+                    && !items[i].name.equals("Backstage passes for HAXX")) {
+
+                // Kwaliteit kan niet onder 0 gaan
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("B-DAWG Keychain")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
-            } else {
+            }
+            // De items die in de eerste if geskipt worden zijn speciale items waarvan de kwaliteit alleen stijgt
+            else {
+
+                // Kwaliteit kan niet boven de 50 gaan
+                // Legendarische items hebben een kwaliteit van 80 en worden hier ook overgeslagen
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes for Re:Factor") || items[i].name.equals("Backstage passes for HAXX") ) {
+                    // Speciale regels voor Backstage passes
+                    if (items[i].name.equals("Backstage passes for Re:Factor") || items[i].name.equals("Backstage passes for HAXX")) {
+
+                        // Kwaliteit verhoogt nogmaals als sellIn 10 dagen of minder is & kwaliteit onder de 50
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
+                        // Kwaliteit verhoogt nogmaals als sellIn 5 dagen of minder is & kwaliteit onder de 50
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -38,22 +52,32 @@ class GildedTros {
                 }
             }
 
+            // Legendarisch item moet nooit verkocht worden
+            // Andere items moeten wel verkocht worden dus elke dag verlaagt de sellIn met 1
             if (!items[i].name.equals("B-DAWG Keychain")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
+            // SellIn onder 0 -> kwaliteit gaat dubbel zo snel achteruit
             if (items[i].sellIn < 0) {
+                // Goede wijn wordt alleen maar beter met de jaren
                 if (!items[i].name.equals("Good Wine")) {
+
+                    // Kwaliteit van Backstage passes wordt ook niet verlaagd
                     if (!items[i].name.equals("Backstage passes for Re:Factor") && !items[i].name.equals("Backstage passes for HAXX")) {
+                        // Kwaliteit kan niet onder 0 gaan
                         if (items[i].quality > 0) {
+                            // Legendarische items kunnen niet zakken in kwaliteit
                             if (!items[i].name.equals("B-DAWG Keychain")) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        // Voor Backstage passes wordt de kwaliteit op 0 gezet als deze verlopen
+                        items[i].quality = 0;
                     }
                 } else {
+                    // Voor goede wijn stijgt de kwaliteit tot 50
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
