3c9381014f7515b82a3a4dab0246f1c8fe72a23b
IebeMaes <Iebe.maes@outlook.com>
2023-11-26 11:02:32 +0100

    kleine refactoring van ItemConverter: weghalen van onnodige constantes

--- Java/src/main/java/com/gildedtros/ItemConverter.java
+++ Java/src/main/java/com/gildedtros/ItemConverter.java
@@ -5,14 +5,10 @@ import com.gildedtros.category.*;
 import java.util.List;
 
 public class ItemConverter {
-
-    private static final String LEGENDARY_ITEM = "B-DAWG Keychain";
-    private static final String GOOD_WINE = "Good Wine";
-
     public static ItemCategory getItemCategory(Item item) {
         return switch (item.name) {
-            case GOOD_WINE -> new GoodWine();
-            case LEGENDARY_ITEM -> new Legendary();
+            case "Good Wine" -> new GoodWine();
+            case "B-DAWG Keychain" -> new Legendary();
             case "Backstage passes for Re:Factor", "Backstage passes for HAXX" -> new InterestingConference();
             case "Duplicate Code", "Long Methods", "Ugly Variable Names" -> new Smelly();
             default -> new Normal();
